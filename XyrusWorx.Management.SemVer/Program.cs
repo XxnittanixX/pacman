607be375a607aca9bc2cd5103f8a62302ed8e2d6
Georg Kiehne <georg.kiehne@xyrus-worx.org>
2017-02-16 00:39:18 +0100

    + Added SemVer tool project

---
+++ XyrusWorx.Management.SemVer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XyrusWorx.Management.SemVer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
