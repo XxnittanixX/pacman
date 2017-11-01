811560c56d936e3b681ddee29c06d54b3169ef78
Georg Kiehne <georg.kiehne@xyrus-worx.org>
2017-11-01 15:21:52 +0100

    Updated cs-library template to be able to be built out of the box

--- tools/pacman/templates/cs-library/init.ps1
+++ tools/pacman/templates/cs-library/init.ps1
@@ -1,3 +1,15 @@
-mv `
-    (join-path $_.Directory.FullName "project.csproj") `
-    (join-path $_.Directory.FullName "$($_.Directory.Name).csproj") 
+Move-Item `
+    -Path        (join-path $_.Directory.FullName "project.csproj") `
+    -Destination (join-path $_.Directory.FullName "$($_.Directory.Name).csproj") 
+
+$properties = New-PropertyContainer (Join-Path $_.Directory.FullName "package.json")
+$properties.setProperty("pacman", @{
+    "build" = @{
+        "projects" = "*.csproj"
+        "target" = "Restore,Pack"
+        "parameters" = @{
+            "configuration" = "Release"
+            "platform" = "Any CPU"
+        }
+    }
+})
